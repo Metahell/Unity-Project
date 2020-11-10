d654129b581e9db4419507115af1c778bacf67b7
Metahell <71194717+Metahell@users.noreply.github.com>
2020-11-10 16:26:44 +0100

    Base de l'UI

---
+++ DJVObjectPoolingCorrection/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Option()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Return()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Leave()
+    {
+        EditorApplication.Exit(0);
+    }
+}
