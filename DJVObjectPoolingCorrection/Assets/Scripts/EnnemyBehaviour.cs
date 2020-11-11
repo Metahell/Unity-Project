13e1ba018eaba53dcc879688459b04cc15f7f28c
Novanef <trios.timothe@hotmail.fr>
2020-11-11 16:11:44 +0100

    arg

---
+++ DJVObjectPoolingCorrection/Assets/Scripts/EnnemyBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyBehaviour : MonoBehaviour
+{
+    int velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3[] positionArray = new [] { new Vector3(1f,0f,0f), new Vector3(-1f,0f,0f),new Vector3(0f,1f,0f),new Vector3(0f,-1f,0f),new Vector3(0f,0f,1f),new Vector3(0f,0f,-1f) }
+        int direction = Random.range(0,6);
+        transform.
+    }
+}
