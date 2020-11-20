74a9dbf300536f20a624d38561c1f764bee46c09
Novanef <trios.timothe@hotmail.fr>
2020-11-20 12:23:05 +0100

    Le soldat nous attaque quand il est proche

--- Assets/Scripts/KnightBehaviour.cs
+++ Assets/Scripts/KnightBehaviour.cs
@@ -17,32 +17,49 @@ public class KnightBehaviour : MonoBehaviour
     private float acceleration;
     [SerializeField]
     private float decceleration;
+    private Vector3 playerPosition;
+    private bool is_moving = true;
     Vector3[] positionArray = new[] { Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
-    [SerializeField] private bool is_moving;
-    private float lerpSmooth;
     // Start is called before the first frame update
     void Start()
     {
         rigi = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Confined;
-        is_moving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerPosition = GameObject.FindGameObjectsWithTag("Player")[0].transform.position;
+        if (Vector3.Distance(playerPosition,transform.position) < 2.5f)
+        {
+            if (!animator.GetCurrentAnimatorStateInfo(1).IsTag("1"))
+            { 
+
+                animator.SetTrigger("Attack");
+            }
+            is_moving = false;
+        }
+        else
+        {
+            is_moving = true;
+            mouvementVector = (transform.forward).normalized;
+            UpdateAnimator();
+        }
         DoRotation();
-        mouvementVector = (transform.forward).normalized;
-        UpdateAnimator();
     }
 
     private void FixedUpdate()
     {
-        motionVector = new Vector3(mouvementVector.x * maxVelocity, rigi.velocity.y, mouvementVector.z * maxVelocity);
-        float lerpSmooth = rigi.velocity.magnitude < motionVector.magnitude ? acceleration : decceleration;
-        rigi.velocity = Vector3.Lerp(rigi.velocity, motionVector, lerpSmooth / 20);
-        if (direction.magnitude > 0)
-            transform.forward = Vector3.Lerp(transform.forward, direction, .3f);
+            if (!animator.GetCurrentAnimatorStateInfo(1).IsTag("1"))
+            {
+            motionVector = new Vector3(mouvementVector.x * maxVelocity, rigi.velocity.y, mouvementVector.z * maxVelocity);
+            float lerpSmooth = rigi.velocity.magnitude < motionVector.magnitude ? acceleration : decceleration;
+            rigi.velocity = Vector3.Lerp(rigi.velocity, motionVector, lerpSmooth / 20);
+
+            if (direction.magnitude > 0)
+                transform.forward = Vector3.Lerp(transform.forward, direction, .3f);
+        }
     }
 
     private void UpdateAnimator()
@@ -54,7 +71,6 @@ public class KnightBehaviour : MonoBehaviour
 
     private void DoRotation()
     {
-        Vector3 playerPosition = GameObject.FindGameObjectsWithTag("Player")[0].transform.position;
         playerPosition.y = transform.position.y;
         direction = playerPosition - transform.position;
         direction.y = 0;
