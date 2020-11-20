d7c71f31900eabfcd43110d1f8097e1c26de5805
Novanef <trios.timothe@hotmail.fr>
2020-11-20 10:34:08 +0100

    le chevalier suit le joueur

--- Assets/Scripts/KnightBehaviour.cs
+++ Assets/Scripts/KnightBehaviour.cs
@@ -7,8 +7,7 @@ public class KnightBehaviour : MonoBehaviour
     [Header("Link")]
     private Rigidbody rigi;
     [SerializeField] private Animator animator;
-    public Transform targetDirection;
-    [SerializeField]  private Vector3 mouvementVector = Vector3.zero;
+    [SerializeField] private Vector3 mouvementVector = Vector3.zero;
     private Vector3 motionVector = Vector3.zero;
     private Vector3 direction;
     [Header("Movement Parameters")]
@@ -18,7 +17,7 @@ public class KnightBehaviour : MonoBehaviour
     private float acceleration;
     [SerializeField]
     private float decceleration;
-    Vector3[] positionArray = new[] { Vector3.left, Vector3.right,Vector3.forward,Vector3.back };
+    Vector3[] positionArray = new[] { Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
     [SerializeField] private bool is_moving;
     private float lerpSmooth;
     // Start is called before the first frame update
@@ -26,74 +25,22 @@ public class KnightBehaviour : MonoBehaviour
     {
         rigi = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Confined;
-        is_moving = false;   
+        is_moving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         DoRotation();
-        StartCoroutine("pause");
-    }
-
-    IEnumerator pause()
-    {
-        if (!is_moving)
-        {
-            is_moving = true;
-
-            mouvementVector = Vector3.zero;
-            motionVector = new Vector3(mouvementVector.x * maxVelocity, rigi.velocity.y, mouvementVector.z * maxVelocity);
-            lerpSmooth = rigi.velocity.magnitude < motionVector.magnitude ? acceleration : decceleration;
-            rigi.velocity = Vector3.Lerp(rigi.velocity, motionVector, lerpSmooth / 20);
-            if (direction.magnitude > 0)
-                transform.forward = Vector3.Lerp(transform.forward, direction, .3f);
-            UpdateAnimator();
-            yield return new WaitForSeconds(0.1f);
-        mouvementVector = positionArray[Random.Range(0, 4)];
-        StartCoroutine("move");
-
-        }
-        
-        yield return null;
-    }
-
-    IEnumerator move()
-    {
-        if (is_moving)
-        {
-            for (float i = 0; i < 4; i += Time.deltaTime)
-            {
-                motionVector = new Vector3(mouvementVector.x * maxVelocity, rigi.velocity.y, mouvementVector.z * maxVelocity);
-                lerpSmooth = rigi.velocity.magnitude < motionVector.magnitude ? acceleration : decceleration;
-                rigi.velocity = Vector3.Lerp(rigi.velocity, motionVector, lerpSmooth / 20);
-                if (direction.magnitude > 0)
-                    transform.forward = Vector3.Lerp(transform.forward, direction, .3f);
-                UpdateAnimator();
-                yield return null;
-            }
-
-            mouvementVector = Vector3.zero;
-            motionVector = new Vector3(mouvementVector.x * maxVelocity, rigi.velocity.y, mouvementVector.z * maxVelocity);
-            lerpSmooth = rigi.velocity.magnitude < motionVector.magnitude ? acceleration : decceleration;
-            rigi.velocity = Vector3.Lerp(rigi.velocity, motionVector, lerpSmooth / 20);
-            if (direction.magnitude > 0)
-                transform.forward = Vector3.Lerp(transform.forward, direction, .3f);
-            UpdateAnimator();
-            is_moving = false;
-            
-        }
-        mouvementVector = Vector3.zero;
-        motionVector = new Vector3(mouvementVector.x * maxVelocity, rigi.velocity.y, mouvementVector.z * maxVelocity);
-        lerpSmooth = rigi.velocity.magnitude < motionVector.magnitude ? acceleration : decceleration;
-        rigi.velocity = Vector3.Lerp(rigi.velocity, motionVector, lerpSmooth / 20);
-        if (direction.magnitude > 0)
-            transform.forward = Vector3.Lerp(transform.forward, direction, .3f);
+        mouvementVector = (transform.forward).normalized;
         UpdateAnimator();
     }
 
     private void FixedUpdate()
     {
+        motionVector = new Vector3(mouvementVector.x * maxVelocity, rigi.velocity.y, mouvementVector.z * maxVelocity);
+        float lerpSmooth = rigi.velocity.magnitude < motionVector.magnitude ? acceleration : decceleration;
+        rigi.velocity = Vector3.Lerp(rigi.velocity, motionVector, lerpSmooth / 20);
         if (direction.magnitude > 0)
             transform.forward = Vector3.Lerp(transform.forward, direction, .3f);
     }
