93e3d8cc4f036aeee48ed6df39f30f7eaa95fbb4
Metahell <71194717+Metahell@users.noreply.github.com>
2020-11-23 16:07:15 +0100

    Update WaveManager.cs

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
+    private int WaveID = 1;
     private int LastWave = 0;
     private int CurrentWave = 3;
     [SerializeField]
@@ -19,25 +20,43 @@ public class WaveManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (CheckEnemyCount() == 0)
+        {
+            StartCoroutine("Spawn");
+        }
+        if (WaveID == 11)
+        {
+            Win();
+        }
     }
     private void NewWave()
     {
         int NewWave = CurrentWave + LastWave;
         LastWave = CurrentWave;
         CurrentWave = NewWave;
+        Debug.Log(""+CurrentWave);
     }
     private IEnumerator Spawn()
     {
         yield return new WaitForSeconds(1);
         for(int i = 0; i < CurrentWave; i++)
         {
-            Debug.Log("CurrentWave :" + CurrentWave);
             GameObject mob=Instantiate(EnnemyPool[(i%EnnemyPool.Count)]);
             mob.transform.position = SpawnPoints[i % SpawnPoints.Count].transform.position;
 
             yield return new WaitForSeconds(1);
         }
+        NewWave();
+        WaveID++;
+    }
+    private int CheckEnemyCount()
+    {
+        return GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+    }
+    private void Win()
+    {
+        //Affiche texte, renvoie au menu, update stats de l'écran de sélection des personnages
     }
 }
                                       
