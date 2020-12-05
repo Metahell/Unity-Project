759984d0d4c2efdc991e31faa2f382b5be8fbf29
Metahell <71194717+Metahell@users.noreply.github.com>
2020-12-05 17:36:09 +0100

    Hp Bar Diablo like

---
+++ Assets/Scripts/HealthOrb.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthOrb : MonoBehaviour
+{
+    [SerializeField]
+    private int hpmax=100;
+    private int currentHp;
+    [SerializeField]
+    private GameObject healthOrb;
+    [SerializeField]
+    private Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.maxValue = hpmax;
+        currentHp = hpmax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = currentHp;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentHp -= 1;
+        }
+    }
+    public void UpdateOrb()
+    {
+        healthOrb.transform.position -= new Vector3(0,(slider.maxValue-slider.value)*2.62f/slider.maxValue,0);
+    }
+}
