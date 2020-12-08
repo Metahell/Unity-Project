18f354ad26b44c1e5a44d56e1d83afc34cbef3a3
Novanef <trios.timothe@hotmail.fr>
2020-12-08 14:10:38 +0100

    le soldat devient rouge quand on le touche

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -24,5 +24,27 @@ public class HealthManager : MonoBehaviour
     public void LooseHealth(int healthLoss)
     {
         health -= healthLoss;
+        StartCoroutine("Red");
+    }
+
+    IEnumerator Red()
+    {
+
+
+        Transform cube = gameObject.transform.Find("Cube.002");
+        Material[] materials = cube.GetComponent<Renderer>().materials;
+        Color color0 = materials[0].color;
+        Color color1 = materials[1].color;
+        Color color2 = materials[2].color;
+        Color color3 = materials[3].color;
+        materials[0].color = Color.red;
+        materials[1].color = Color.red;
+        materials[2].color = Color.red;
+        materials[3].color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        materials[0].color = color0;
+        materials[1].color = color1;
+        materials[2].color = color2;
+        materials[3].color = color3;
     }
 }
