318eab99986a6eced38757d40a018734c9cf4a9a
Metahell <71194717+Metahell@users.noreply.github.com>
2020-12-17 17:00:38 +0100

    Tests Menu/Sélection

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> Maps=new List<GameObject>();
+    void Start()
+    {
+        if (ButtonBehavior.MapSelection == 0)
+        {
+            Maps[Random.Range(0, Maps.Capacity)].SetActive(true);
+        }
+        else
+        {
+            Maps[ButtonBehavior.MapSelection-1].SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
